d11d6b6dc9a8cc854dd338896e95130b1f5e856a
kagikn <kagikn.3939@gmail.com>
2015-05-19 21:08:07 +0900

    アーマー支給スクリプトを追加

---
+++ Inferno/InfernoScripts/Player/ArmorAndHealthSupplier.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using GTA;
+using GTA.Native;
+
+namespace Inferno
+{
+    class ArmorAndHealthSupplier : InfernoScript
+    {
+
+        bool isSuppliedOnMission = false;  //ミッション開始してから全回復させたか
+        bool supplyFlag = false;
+
+        bool isActive = false;
+
+        /// <summary>
+        /// 0.1秒間隔
+        /// </summary>
+        protected override int TickInterval
+        {
+            get { return 100; }
+        }
+
+        protected override void Setup()
+        {
+
+            CreateInputKeywordAsObservable("armor")
+                .Subscribe(_ =>
+                {
+                    isActive = !isActive;
+                    DrawText("SupplyArmorAndHealth:" + isActive, 3.0f);
+                });
+
+            OnAllOnCommandObservable.Subscribe(_ => isActive = true);
+
+            OnTickAsObservable
+                .Where(_ => isActive)
+                .Subscribe(_ => 
+                    {
+                        var player = this.GetPlayer();
+
+                        if (player.IsSafeExist())
+                        {
+                            if (CheckSupplyFlag()) { supplyFlag = true; }
+                            if ((supplyFlag && player.IsAlive)) { SupplyArmorAndHealth(); }
+                        }                        
+                    });                  
+        }
+        
+        /// <summary>
+        /// 体力とアーマー回復させるべきかどうか
+        /// </summary>
+        private bool CheckSupplyFlag()
+        {
+            var player = this.GetPlayer();
+            var missionFlag = GetMissionFlag();
+
+            if (!missionFlag) { isSuppliedOnMission = false; }
+
+            if (!player.IsAlive)
+            {
+                return true;
+            }
+
+            if (missionFlag && !isSuppliedOnMission)
+            {
+                isSuppliedOnMission = true; 
+                return true;
+            }
+            return false;
+        } 
+
+        private void SupplyArmorAndHealth()
+        {
+            var player = this.GetPlayer();
+
+            DrawText("The armor was supplied.", 3.0f);
+            supplyFlag = false; 
+
+            var maxHealth = player.MaxHealth;
+            var maxArmor = Game.Player.GetPlayerMaxArmor();
+            player.Health = maxHealth;
+            player.Armor = maxArmor;
+        }
+
+
+        /// <summary>
+        /// 現在ミッション中かどうか 
+        /// </summary>
+        /// (Player.IsOnMissionはミッションを始められる状態かどうかの判定)
+        public bool GetMissionFlag()
+        {
+            var _missionFlag = Function.Call<int>(Hash.GET_MISSION_FLAG);
+            if (_missionFlag != 0) { return true; }
+            else { return false; }
+        }
+    }
+}
