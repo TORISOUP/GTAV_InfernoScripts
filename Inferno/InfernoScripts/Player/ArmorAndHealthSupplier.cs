c72a2a79ac0bb26195058074df7d88760b772b8e
kagikn <kagikn.3939@gmail.com>
2015-05-29 00:46:38 +0900

    条件判定の大半をLINQ文で行うようにした

--- Inferno/InfernoScripts/Player/ArmorAndHealthSupplier.cs
+++ Inferno/InfernoScripts/Player/ArmorAndHealthSupplier.cs
@@ -12,10 +12,7 @@ namespace Inferno
 {
     class ArmorAndHealthSupplier : InfernoScript
     {
-
-        bool isSuppliedOnMission = false;  //ミッション開始してから全回復させたか
-        bool supplyFlag = false;
-
+        bool hasPlayerWasted = false; 
         bool isActive = false;
 
         /// <summary>
@@ -40,43 +37,29 @@ namespace Inferno
 
             OnTickAsObservable
                 .Where(_ => isActive)
+                .Select(_ => this.GetPlayer())
+                .Where(p => p.IsSafeExist())
+                .Select(p => p.IsDead)
+                .DistinctUntilChanged()
+                .Subscribe(flag =>
+                {
+                    if  (flag) { hasPlayerWasted = true; }
+                });
+
+            OnTickAsObservable
+                .Where(_ => isActive)
+                .Select(_ => this.GetPlayer())
+                .Where(p => p.IsSafeExist())
+                .Select(p => GetMissionFlag() || (p.IsAlive && hasPlayerWasted))
+                .DistinctUntilChanged()
+                .Where(flag => flag)
                 .Subscribe(_ =>
-                    {
-                        var player = this.GetPlayer();
-
-                        if (player.IsSafeExist())
-                        {
-                            if (CheckSupplyFlag())
-                            {
-                                supplyFlag = true;
-                                if (!isSuppliedOnMission) { isSuppliedOnMission = true; }
-                            }
-                            if ((supplyFlag && player.IsAlive)) { SupplyArmorAndHealth(); }
-                        }
-                    });
-
-
+                {
+                    SupplyArmorAndHealth();
+                    hasPlayerWasted = false;
+                });
         }
         
-        /// <summary>
-        /// 体力とアーマーを回復させるタイミングかどうか
-        /// </summary>
-        private bool CheckSupplyFlag()
-        {
-            var player = this.GetPlayer();
-
-            if (player.IsSafeExist() && !player.IsAlive)
-            {
-                return true;
-            }
-
-            if (GetMissionFlag() && !isSuppliedOnMission)
-            {
-                return true;
-            }
-            return false;
-        }
-
         /// <summary>
         /// 体力とアーマー回復
         /// </summary>
@@ -85,7 +68,6 @@ namespace Inferno
             var player = this.GetPlayer();
 
             DrawText("The armor was supplied.", 3.0f);
-            supplyFlag = false; 
 
             var maxHealth = player.MaxHealth;
             var maxArmor = Game.Player.GetPlayerMaxArmor();
