f09b9d4545b76d0915accc2878e1f12c4dcd97e6
TORISOUP <tori.birdstrike@gmail.com>
2015-09-01 00:31:57 +0900

    自殺するとバクハツシサンするようにした

--- Inferno/InfernoScripts/Player/KillPlayer.cs
+++ Inferno/InfernoScripts/Player/KillPlayer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GTA;
+using GTA.Math;
 
 namespace Inferno
 {
@@ -14,7 +16,18 @@ namespace Inferno
         protected override void Setup()
         {
             CreateInputKeywordAsObservable("killme")
-                .Subscribe(_ => playerPed.Kill());
+                .Subscribe(_ =>
+                {
+                    World.AddExplosion(playerPed.Position, GTA.ExplosionType.SmallExplosion1, 10.0f, 0.1f);
+                    playerPed.Kill();
+                    //自殺コマンドで死んだときはランダムな方向にふっとばす
+                    var x = Random.NextDouble() - 0.5;
+                    var y = Random.NextDouble() - 0.5;
+                    var z = Random.NextDouble() - 0.5;
+                    var randomVector = new Vector3((float)x, (float)y, (float)z);
+                    randomVector.Normalize();
+                    playerPed.ApplyForce(randomVector*100);
+                });
         }
     }
 }
