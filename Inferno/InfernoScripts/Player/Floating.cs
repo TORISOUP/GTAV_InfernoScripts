40cd1432e0998a9cf23232fb4cd1d2bab7d23a9c
TORISOUP <tori.birdstrike@gmail.com>
2016-01-09 14:04:08 +0900

    ふわジャン作成

---
+++ Inferno/InfernoScripts/Player/Floating.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA.Math;
+using UniRx;
+
+namespace Inferno.InfernoScripts.Player
+{
+    //ふわふわジャンプ
+    class Floating : InfernoScript
+    {
+        protected override int TickInterval { get; } = 50;
+
+        protected override void Setup()
+        {
+            this.OnTickAsObservable
+                .Where(_ => this.IsGamePadPressed(GameKey.Sprint) && this.IsGamePadPressed(GameKey.Jump))
+                .Subscribe(_ =>
+                {
+                    PlayerPed.ApplyForce(Vector3.WorldUp *1.1f );
+                });
+        }
+    }
+}
