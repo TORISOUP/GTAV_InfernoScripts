bcab151cf2a30f88f11fae19f267dd3663406f0a
meronmks <meronmks.8914@gmail.com>
2019-03-30 18:36:03 +0900

    野球大会

---
+++ Inferno/InfernoScripts/Parupunte/Scripts/BaseballTournament.cs
@@ -0,0 +1,31 @@
+﻿using Inferno.InfernoScripts.Event.ChasoMode;
+using UniRx;
+
+namespace Inferno.InfernoScripts.Parupunte.Scripts
+{
+    /// <summary>
+    /// 野球大会
+    /// </summary>
+    [ParupunteConfigAttribute("野球大会", "おわり")]
+    [ParupunteIsono("やきゅう")]
+    class BaseballTournament : ParupunteScript
+    {
+        public BaseballTournament(ParupunteCore core, ParupunteConfigElement element) : base(core, element)
+        {
+        }
+
+        public override void OnStart()
+        {
+            ReduceCounter = new ReduceCounter(20 * 1000);
+            AddProgressBar(ReduceCounter);
+            ReduceCounter.OnFinishedAsync.Subscribe(_ => ParupunteEnd());
+
+            this.OnFinishedAsObservable.Subscribe(_ =>
+            {
+                Inferno.InfernoCore.Publish(ChasoModeEvent.SetToDefault);
+            });
+
+            Inferno.InfernoCore.Publish(new ChangeWeaponEvent(Weapon.BAT));
+        }
+    }
+}
